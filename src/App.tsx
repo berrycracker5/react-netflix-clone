8b7bde1c3f71d4148c4330377d1f19c6000e8ba3
JW-GalaxyBook <wlsdhks5495@naver.com>
2023-12-01 17:03:19 +0900

    231201 : 강의 Follow 중단 - CSS 가 제공되지 않음 - 뒤에 내용을 보니 이미 아는 것이 많음 - 차라리 CSS 강의를 듣는게 낫다고 판단

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,32 @@ import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import Banner from "./components/Banner";
+import Row from "./components/Row";
+import TMDB_REQEUST from "./api/request";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
-      {/* <Row /> */}
-      {/* <Row /> */}
-      {/* <Row /> */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={TMDB_REQEUST.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={TMDB_REQEUST.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={TMDB_REQEUST.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={TMDB_REQEUST.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={TMDB_REQEUST.fetchComedyMovies}
+      />
       {/* <Footer /> */}
     </div>
   );
