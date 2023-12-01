0e7a3c95336d0832ffd2dbce3961bb249b0e884b
JW-GalaxyBook <wlsdhks5495@naver.com>
2023-12-01 15:48:45 +0900

    2312201 : 배너에서 재생 클릭시 비디오 완성 - 일단 맞는 소스인데, youtube 정책인지 영상 재생은 불가능함 - styled component 설치 : css를 tsx안에서 작성할 수 있음 - CSS 문법에서 & 연산자는, 위 부모 클래스를 그대로 가져온다는 뜻 - ::after 연산자는 렌더링 후 가상요소를 삽입해주는 것임 (content:"asdf"를 입력하면 asdf가 보이게 됨)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./Banner.css";
 import TMDB_REQEUST from "../api/request";
 import axiosInstance from "../api/axios";
+import styled from "styled-components";
 
 function Banner() {
   const [movie, setMovie] = useState<any>([]);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
 
   useEffect(() => {
     fetchData();
@@ -36,35 +38,90 @@ function Banner() {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   };
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage:
-          movie?.backdrop_path &&
-          `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage:
+            movie?.backdrop_path &&
+            `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
 
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">More Infomation</button>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => {
+                setIsClicked(true);
+              }}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Infomation</button>
+          </div>
+
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 100)}
+          </h1>
         </div>
 
-        <h1 className="banner__description">
-          {truncate(movie?.overview, 100)}
-        </h1>
-      </div>
-
-      <div className="banner--fadeBottom"></div>
-    </header>
-  );
+        <div className="banner--fadeBottom"></div>
+      </header>
+    );
+  } else {
+    return (
+      <>
+        <Container>
+          <HomeContainer>
+            <IFrame
+              src={`https://www.youtube.com/embed/${movie?.videos?.results?.[0]?.key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie?.videos?.results?.[0]?.key}}`}
+              width="640"
+              height="360"
+              allow="autoplay; fullscreen;"
+            ></IFrame>
+          </HomeContainer>
+        </Container>
+      </>
+    );
+  }
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const IFrame = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default Banner;
